730e91500e10418adfb2ab3248f37a94e952ac29
JosueMendezS <MARVIN.MENDEZSANABRIA@ucr.ac.cr>
2026-09-16 15:39:29 -0600

    initial commit

---
+++ logiexpress/src/main/java/cr/ac/ucr/paraiso/ie/c5h060/logiexpress/LogiexpressApplication.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.paraiso.ie.c5h060.logiexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogiexpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogiexpressApplication.class, args);
+	}
+
+}
---
+++ logiexpress/src/test/java/cr/ac/ucr/paraiso/ie/c5h060/logiexpress/LogiexpressApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.paraiso.ie.c5h060.logiexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogiexpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
